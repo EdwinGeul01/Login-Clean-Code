d21cddfe65007f735522a686235e98b4227eaf61
Edwin Geul <edwingeul01@gmail.com>
2022-11-07 13:12:41 -0600

    Agregar archivos de proyecto.

---
+++ Login/Program.cs
@@ -0,0 +1,156 @@
+﻿/*
+ Elegancia operador ternario 
+ Números mágicos 
+Complejidad condicional 
+Polimorfismo vs Enum 
+Ser declarativo
+Método de tabla
+
+ */
+
+
+
+using Login_CleanCode;
+
+
+/*
+ ************************************************************************************************************************************************************************************************
+ */
+
+
+
+
+
+
+
+
+/*
+ ************************************************************************************************************************************************************************************************
+ */
+
+// NO USING CLEAN CODE
+
+
+class LogMng
+{
+    string[,] users = new string[,] { 
+        { "user", "pass", "active", "admin" },
+        { "user2", "pass2", "active", "normal"}
+    };
+
+
+    private void LoginUserNormal()
+    {
+        Console.WriteLine("Has iniciado como usuario normal");
+        responsibilities.Showresponsibilities(1);
+    }
+
+
+    private void loginUserAdmin()
+    {
+
+        Console.WriteLine("Has iniciado como administrador");
+        responsibilities.Showresponsibilities(0);
+
+
+    }
+
+
+
+
+
+    public void Log(string u , string p)
+    {
+        for (int i = 0; i < users.Length; i++)
+        {
+            if (users[i,0] == u
+                && users[i,1] == p
+                && users[i,2] == "active")
+            {
+
+                if (users[i,3] == "admin")
+                {
+                    loginUserAdmin();
+
+                }else if(users[i,3] == "normal")
+                {
+                    LoginUserNormal();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(
+                "Credenciales ingresadas invalidas , revisa si tu usuario ," +
+                " contraseña o si este usuario esta [ activo ]");
+
+            }    
+
+        }
+
+    }
+
+    
+
+
+
+}
+
+class responsibilities
+{
+
+    public static void Showresponsibilities(int rol)
+    {
+        if(rol == 0)
+        {
+            Console.WriteLine("administrate the databases");
+        }
+        if(rol == 1)
+        {
+            Console.WriteLine("no responsibility");
+        }
+
+
+    }
+
+
+
+}
+
+/*
+ ************************************************************************************************************************************************************************************************
+ */
+
+
+
+
+
+
+class MainClass
+{
+
+    
+
+    public static void Main(string[] args)
+    {
+        LoginManager loginManager = new LoginManager();
+        string user, pass;
+
+
+        Console.WriteLine("___Login___");
+        Console.Write("User: ");
+        user = Console.ReadLine();
+        Console.Write("Pass: ");
+        pass = Console.ReadLine();
+
+        //logMng.Log(user, pass);
+       loginManager.login(user, pass);
+
+
+
+    }
+
+
+
+
+}
---
+++ Login/User.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_CleanCode
+{
+    abstract class user
+    {
+        protected user()
+        {
+
+        }
+        protected user(string _username, string _password, estado _status)
+        {
+            username = _username;
+            password = _password;
+            AcountStatus = _status;
+
+        }
+
+
+        public enum estado { active, inactive };
+        public enum UserType { admin, normal };
+        public string username;
+        public string password;
+        public estado AcountStatus;
+        public abstract void Login();
+
+    }
+
+    class adminUser : user
+    {
+        public adminUser()
+        { }
+        public adminUser(string _username, string _password, estado _status) : base(_username, _password, _status)
+        {
+            username = _username;
+            password = _password;
+            AcountStatus = _status;
+
+        }
+
+        public override void Login()
+        {
+            Console.WriteLine("Has iniciado como administrador");
+            Console.WriteLine("Tus responsabilidades son : ");
+            Console.WriteLine(UserResponsibilities.Showresponsibilities("admin") + '\n');
+        }
+    }
+    class normalUser : user
+    {
+        public normalUser(string _username, string _password, estado _status) : base(_username, _password, _status)
+        {
+            username = _username;
+            password = _password;
+            AcountStatus = _status;
+        }
+
+        override public void Login()
+        {
+            Console.WriteLine("Has iniciado como usuario normal");
+            Console.WriteLine("Tus responsabilidades son : ");
+            Console.WriteLine(UserResponsibilities.Showresponsibilities("normal") + '\n');
+        }
+
+    }
+
+
+    static class UserResponsibilities
+    {
+        static Dictionary<string, string> responsibilities = new Dictionary<string, string>
+        {
+            ["normal"] = "no responsibility",
+            ["admin"] = "administrate the databases"
+
+        };
+        
+
+
+        public static string Showresponsibilities(string rol)
+        {
+                return responsibilities[rol];
+        }
+
+
+
+    }
+
+
+
+    class LoginManager
+    {
+        List<user> users = new List<user>();
+
+        public LoginManager()
+        {
+            users.Add(new adminUser("user", "pass", user.estado.active));
+            users.Add(new normalUser("user2", "pass2", user.estado.active));
+
+
+        }
+
+
+        public void login(string username, string password)
+        {
+            bool IsLogged = false;
+            String InvalidSessionMessage = "Invalid entered credentials, check if your username," +
+                " password or if this user is [ active ]";
+            String LoggedSucesffulMessage = "You have successfully logged in";
+
+            foreach (user user in users)
+            {
+                bool IsUserValid = (
+                        user.username == username
+                        && user.password == password
+                        && user.AcountStatus == user.estado.active
+                     );
+
+                if (IsUserValid)
+                {
+                    user.Login();
+                    IsLogged = true;
+                }
+            }
+
+            Console.WriteLine(IsLogged ? LoggedSucesffulMessage : InvalidSessionMessage);
+        }
+
+
+    }
+}
